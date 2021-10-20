76a6b1507065d3a971c4dc9d2602a104443ddefc
KevinShredds <39281059+KevinShredds@users.noreply.github.com>
2021-10-20 13:20:46 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import Web3 from "web3";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Web3Test';
+export class AppComponent implements OnInit {
+  title = 'ngEth';
+   
+  
+  private window: any;
+  private address: any;
+  
+  constructor(@Inject(DOCUMENT) private document: Document)
+  {
+    this.window = this.document.defaultView;
+  }
+
+  public onConnectBtnClick() {
+    if (this.window.ethereum) {
+      this.window.web3 = new Web3(this.window.ethereum);
+      this.window.ethereum.enable();
+      //this.address = this.window.web3.getAddress();
+      console.log(this.window.web3.eth);
+
+      return true;
+    }
+  }
+  
+  ngOnInit() {
+    
+       
+  }
+  
 }
