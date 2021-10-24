d33c4c4b1c6e270482c8279b312acad8af2d4f00
KevinShredds <39281059+KevinShredds@users.noreply.github.com>
2021-10-24 09:16:31 +0200

    added network change listener

--- src/app/transfer/transfer/transfer.component.ts
+++ src/app/transfer/transfer/transfer.component.ts
@@ -12,33 +12,28 @@ declare let window: any;
   styleUrls: ['./transfer.component.css']
 })
 export class TransferComponent implements OnInit {
-
-  private account: any = null;
-  
-  private enable: any;
-
-
+ 
   public user:any;
-  public balance = 0;
-  public accountName = "Metamask is not connected";
+  public balance: number = 0;
+  public accountName: string = "Metamask is not connected";
+  public connectedNetwork = "";
 
 
   constructor() { }
 
-  public getAccountAndBalance() {
+  public getAccountAndBalance(): void {
       console.log("getAccounts from getAccountAndBalance")
-      window.web3.eth.getAccounts().then((accounts) => {
-        console.log(accounts);
+      window.web3.eth.getAccounts().then((accounts) => {        
         document.getElementById('accountName').innerHTML = accounts[0];
-        window.web3.eth.getBalance(accounts[0]).then((balance) => {
-          console.log(balance);
+        window.web3.eth.getBalance(accounts[0]).then(balance => {         
           document.getElementById('balance').innerHTML = balance;
+          document.getElementById('network').innerHTML = this.getChainName(window.ethereum.chainId);
         })
       })
       
   }
 
-  public connectWallet() {
+  public connectWallet(): void {
     
     if (window.ethereum === undefined) {
       alert('Non-Ethereum browser detected. Install MetaMask');
@@ -51,6 +46,46 @@ export class TransferComponent implements OnInit {
       
     }
   }
+
+  public getChainName(chainId: string): string {
+    switch (chainId) {
+      case "0x1":
+        return "Ethereum Mainnet";
+        break;
+
+      case "0x3":
+        return "Ropsten Testnet"
+        break;
+
+      case "0x2a":
+        return "Kovan Testnet"
+        break;
+
+      case "0x4":
+        return "Rinkeby Testnet"
+        break;
+      
+      case "0x5":
+        return "Goerli Testnet"
+        break;
+      
+      case "0x89":
+        return "Matic Network"
+        break;
+
+      case "0x61":
+        return "Binance Smart Chain Testnet"
+        break;
+
+        case "0x38":
+          return "Binance Smart Chain"
+          break;
+        
+      default:
+        return "Unknown Network"
+        break;
+    }
+  }
   
 
   ngOnInit(): void {
@@ -59,6 +94,13 @@ export class TransferComponent implements OnInit {
 
     window.ethereum.on('accountsChanged', (accounts) => {
       console.log('event :: accountsChanged')
+      this.getAccountAndBalance();
+
+    });
+
+    window.ethereum.on('chainChanged', (chainId) => {
+      console.log('event :: chainChanged');
+      console.log(chainId);
       this.getAccountAndBalance();
 
     });
